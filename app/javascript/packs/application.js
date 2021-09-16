750168543ea587febc0c569fef856ae3cbe9fb89
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2021-09-17 01:10:51 +0200

    Added JS and CSS assets and book scaffold

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "bootstrap";
+import "../../assets/stylesheets/application";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
