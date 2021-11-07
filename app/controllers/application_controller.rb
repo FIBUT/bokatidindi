64cb3319bcd1a9510257847a9525461641b74e01
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2021-11-08 00:36:37 +0100

    Redirecting requests to www.bokatidindi.is

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_root_domain
+
+  private
+
+  def redirect_root_domain
+    return nil unless request.host == 'bokatidindi.is'
+
+    redirect_to(
+      "#{request.protocol}www.bokatidindi.is#{request.fullpath}",
+      status: 301
+    )
+  end
 end
