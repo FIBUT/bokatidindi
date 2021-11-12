efa1961ee1f09a7addc3583e14c910c0b6642a96
Alda Vigdis Skarphedinsdottir <191583+aldavigdis@users.noreply.github.com>
2021-11-13 00:09:03 +0100

    Initialising content etc. for about page (#138)
    * Initialising content etc. for about page
    
    * Adding PDF to the assets folder (will end up in the CDN)
    
    * Adding content to the about page
    
    * About content finalised

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
 class PagesController < ApplicationController
   def um_bokatidindi
+    @image_format = image_format
   end
 
   def privacy_policy
+  end
+
+  private
+
+  def image_format
+    return 'jpg' if browser.ie?
+    return 'jpg' if browser.safari? && browser.platform.mac?('<11.6')
+    return 'jpg' if browser.platform.ios?('<14')
+    return 'jpg' if browser.platform.kai_os?
+
+    'webp'
   end
 end
