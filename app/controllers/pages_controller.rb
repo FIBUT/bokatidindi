aef93e321dc18b2c9211bfc374f8eff91caa7e97
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2021-11-12 20:19:57 +0000

    Adding controller for subpages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def um_bokatidindi
+  end
+
+  def privacy_policy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get 'pages/um_bokatidindi'
+  get 'pages/privacy_policy'
+
   resources :books, path: 'baekur', only: [:index]
   resources :books, path: 'bok', param: :slug, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
