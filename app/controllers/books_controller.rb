538ba57228d3645fcf932f480bca24cb95bdd5e3
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2021-11-13 18:39:40 +0000

    Adding return statements to the 404 renders
    This fixes #149.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
       @title_tag = "Bókatíðindi - Leitarniðurstöður - #{params[:search]}"
       @books = Book.search(params[:search])
       if @books.length == 1
-        redirect_to book_path(@books.first.slug)
+        return redirect_to book_path(@books.first.slug)
       end
     else
       @title_tag = "Bókatíðindi"
@@ -17,7 +17,7 @@ class BooksController < ApplicationController
       if params[:category]
         @category = Category.find_by(slug: params[:category])
         unless @category
-          render file: 'public/404.html', status: 404, layout: false
+          return render file: 'public/404.html', status: 404, layout: false
         end
 
         @title_tag << " - Flokkur - #{@category.name_with_group}"
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
       if params[:publisher]
         @publisher = Publisher.find_by(slug: params[:publisher])
         unless @publisher
-          render file: 'public/404.html', status: 404, layout: false
+          return render file: 'public/404.html', status: 404, layout: false
         end
 
         @title_tag << " - Útgefandi - #{@publisher.name}"
@@ -41,7 +41,7 @@ class BooksController < ApplicationController
       if params[:author]
         @author = Author.find_by(slug: params[:author])
         unless @author
-          render file: 'public/404.html', status: 404, layout: false
+          return render file: 'public/404.html', status: 404, layout: false
         end
 
         @title_tag << " - Höfundur - #{@author.name}"
