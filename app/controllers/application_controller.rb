2e211524b315043e1f5d74af8da6f352440b33a4
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-05-26 21:26:27 +0200

    Improving the redirect mechanism

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+  REDIRECT_FROM = ['bokatidindi.is', 'bokatidindi-staging.herokuapp.com'].freeze
+
   before_action :redirect_root_domain
 
   private
 
   def redirect_root_domain
-    return nil unless request.host == 'bokatidindi.is' || request.host == 'bokatidindi-staging.herokuapp.com'
+    return nil unless REDIRECT_FROM.include?(request.host)
 
     redirect_to(
       "#{request.protocol}www.bokatidindi.is#{request.fullpath}",
