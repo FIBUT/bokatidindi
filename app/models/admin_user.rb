010995ffb19ef643224d7a35925d6a5f949aad4b
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-07-23 02:41:26 +0200

    Adding minimal ActiveAdmin installation - Database migrations for users and comments - ActiveAdmin resources - Initialisers with some minimal configuration - Minimal locale files - Minimal access denied error - Changes to Sass files to prevent the inclusion of ActiveAdmin's styles - Publishers are now ordered alphabetically by default

---
+++ app/models/admin_user.rb
@@ -0,0 +1,20 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :trackable,
+         :recoverable, :rememberable, :validatable, :lockable
+
+  belongs_to :publisher, optional: true
+
+  enum :role, [:guest, :publisher, :admin]
+
+  validates :name, :email, presence: true
+  validates :password, :password_confirmation, presence: true, on: :create
+  validates :password, confirmation: true
+
+  validates :role, presence: true
+
+  validates :publisher, presence: true, if: :publisher?
+  validates :publisher, absence: true, if: :admin?
+  validates :publisher, absence: true, if: :guest?
+end
