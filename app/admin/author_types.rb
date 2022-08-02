9f9697b59dca6359d9389b5a7059aefbf60b1304
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-08-03 00:19:38 +0200

    Adding ActiveAdmin resource for author types

---
+++ app/admin/author_types.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register AuthorType do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :source_id, :name, :slug, :rod
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:source_id, :name, :slug, :rod]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  permit_params :name, :rod
+
+  config.sort_order = 'rod_asc'
+  config.filters = false
+
+  index do
+    selectable_column
+    column :name do |author_type|
+      link_to author_type.name, admin_author_type_path(author_type)
+    end
+    column :rod
+    actions
+  end
+
+  show do
+    panel 'Upplýsingar um hlutverk' do
+      attributes_table_for author do
+        row :name
+        row :rod
+      end
+    end
+  end
+
+  form do |f|
+    f.semantic_errors
+
+    f.inputs 'Upplýsingar um hlutverk' do
+      f.input :name
+      f.input :rod
+    end
+
+    f.actions
+  end
+end
