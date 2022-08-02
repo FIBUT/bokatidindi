cc417f06781b73145ff5e27c7800d24c301de944
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-08-03 01:45:35 +0200

    Adding staging provisions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  REDIRECT_FROM = ['bokatidindi.is', 'bokatidindi-staging.herokuapp.com'].freeze
+  REDIRECT_FROM = ['bokatidindi.is', 'bokatidindi.herokuapp.com'].freeze
 
   before_action :redirect_root_domain
 
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
   def redirect_root_domain
     return nil unless REDIRECT_FROM.include?(request.host)
+    return nil if ENV.include?('STAGING_MODE')
 
     redirect_to(
       "#{request.protocol}www.bokatidindi.is#{request.fullpath}",
