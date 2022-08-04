96db3b6a6ad1a765867bf069e78ab6b9946604c6
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-08-04 19:28:47 +0200

    Fixing migration issues

--- db/migrate/20220803232703_remove_attributes_from_books.rb
+++ db/migrate/20220803232703_remove_attributes_from_books.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class RemoveAttributesFromBooks < ActiveRecord::Migration[7.0]
   def change
-    remove_column(:books, :page_count, type: :integer)
-    remove_column(:books, :minutes, type: :integer)
-    remove_column(:books, :store_url, type: :string)
-    remove_column(:books, :sample_url, type: :string)
-    remove_column(:books, :audio_url, type: :string)
-    remove_column(:books, :book_author_id, type: :string)
+    remove_column :books, :page_count, :integer, index: true
+    remove_column :books, :minutes, :integer, index: true
+    remove_column :books, :store_url, :string
+    remove_column :books, :sample_url, :string
+    remove_column :books, :audio_url, :string
+    remove_column :books, :book_author_id, :string
   end
 end
