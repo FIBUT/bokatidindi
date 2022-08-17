d6e9d4b0d64ebc876819522a1727d7c2cd42fd38
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-08-17 16:05:30 +0200

    Adding validations for book has_many relationships

--- app/models/book_author.rb
+++ app/models/book_author.rb
@@ -5,6 +5,13 @@ class BookAuthor < ApplicationRecord
   belongs_to :author
   belongs_to :author_type
 
+  validates :author, inclusion: {
+    in: Author.all
+  }
+  validates :author_type, inclusion: {
+    in: AuthorType.all
+  }
+
   def name
     [author.firstname, author.lastname].join(' ')
   end
