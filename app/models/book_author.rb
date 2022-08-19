4952b7cc718bd0ea31e58063b0d8fabe111ba114
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-08-19 03:23:09 +0200

    Adding validations and sanitation

--- app/models/book_author.rb
+++ app/models/book_author.rb
@@ -5,12 +5,16 @@ class BookAuthor < ApplicationRecord
   belongs_to :author
   belongs_to :author_type
 
-  validates :author, inclusion: {
-    in: Author.all
-  }
-  validates :author_type, inclusion: {
-    in: AuthorType.all
-  }
+  validates(
+    :author, inclusion: {
+      in: Author.all, unless: proc { |ba| ba.author_type.blank? }
+    }
+  )
+  validates(
+    :author_type, inclusion: {
+      in: AuthorType.all, unless: proc { |ba| ba.author_type.blank? }
+    }
+  )
 
   def name
     [author.firstname, author.lastname].join(' ')
