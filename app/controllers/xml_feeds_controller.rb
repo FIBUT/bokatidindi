4a305c8581a8c7ef200a85431a33a1a3e2d8f7f9
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-08-26 14:02:46 +0200

    Adding DTD and improving the XML feed

--- app/controllers/xml_feeds_controller.rb
+++ app/controllers/xml_feeds_controller.rb
@@ -1,57 +1,33 @@
 # frozen_string_literal: true
 
 class XmlFeedsController < ApplicationController
+  include ActionView::Helpers::AssetUrlHelper
+  include ActionView::Helpers::SanitizeHelper
+
   def edition_for_print
     edition = if params[:id] == 'current'
                 Edition.current.last
               else
                 Edition.find(params[:id])
               end
-    categories = books_by_category(edition[:id])
 
-    builder = Nokogiri::XML::Builder.new do |xml|
+    categories = print_books_by_category(edition[:id])
+
+    builder = Nokogiri::XML::Builder.new(encoding: 'utf-8') do |xml|
+      xml.doc.create_internal_subset('edition', 'SYSTEM',
+                                     asset_url('edition.dtd'))
+
       xml.edition do
-        xml.id edition[:id]
-        xml.title edition[:title]
+        xml.edition_id edition[:id]
+        xml.edition_title edition[:title]
         xml.categories do
           categories.each do |c|
             xml.category do
-              xml.group c[:group]
-              xml.name c[:name]
+              xml.category_group c[:group]
+              xml.category_name c[:name]
               xml.books do
-                c[:books].each do |b|
-                  xml.book do
-                    xml.id b[:id]
-                    xml.slug b[:slug]
-                    xml.pre_title b[:pre_title] unless b[:pre_title].empty?
-                    xml.title b[:title_hypenated]
-                    xml.post_title b[:post_title] unless b[:post_title].empty?
-                    xml.description b[:description]
-                    if b.cover_image?
-                      xml.cover_image do
-                        xml.url b.cover_image_url('jpeg')
-                        xml.image href: "File:///cover_images/#{b[:slug]}.jpg"
-                      end
-                    end
-                    xml.publisher do
-                      xml.id b.publisher[:id]
-                      xml.name b.publisher[:name]
-                    end
-                    xml.authors do
-                      b.book_authors.order(id: :asc).each do |book_author|
-                        xml.author do
-                          xml.id book_author.author[:id]
-                          xml.type book_author.author_type[:name]
-                          xml.name book_author.author[:name]
-                        end
-                      end
-                    end
-                    xml.binding_types do
-                      b.binding_types.order(rod: :asc).each do |binding_type|
-                        xml.binding_type binding_type.name
-                      end
-                    end
-                  end
+                c[:books].each do |book|
+                  book_markup(book, xml)
                 end
               end
             end
@@ -65,17 +41,61 @@ class XmlFeedsController < ApplicationController
 
   private
 
-  def books_by_category(edition_id = nil)
+  def book_markup(book, xml)
+    xml.book do
+      xml.book_id book[:id]
+      xml.slug book[:slug]
+      xml.pre_title book[:pre_title] unless book[:pre_title].empty?
+      xml.title book[:title_hypenated]
+      xml.post_title book[:post_title] unless book[:post_title].empty?
+      xml.description do
+        xml << sanitize(book.short_description.squish, tags: ['em'])
+      end
+      if book.cover_image?
+        # Two paths for the front cover are provided. The full URL for
+        # retreiving each image from the CDN and the expected path it will be
+        # located at for Adobe InDesign to use.
+        xml.cover_image do
+          xml.url book.cover_image_url('jpeg')
+          xml.image href: "File:///cover_images/#{book[:slug]}.jpg"
+        end
+      end
+      xml.publisher do
+        xml.publisher_id book.publisher[:id]
+        xml.publisher_name book.publisher[:name]
+      end
+      xml.authors do
+        book.book_authors.order(id: :asc).each do |book_author|
+          xml.author do
+            xml.author_id book_author.author[:id]
+            xml.author_type book_author.author_type[:name]
+            xml.author_name book_author.author[:name]
+          end
+        end
+      end
+      xml.binding_types do
+        book.binding_types.order(rod: :asc).each do |binding_type|
+          xml.binding_type binding_type.corrected_name
+        end
+      end
+    end
+  end
+
+  def print_books_by_category(edition_id = nil)
     edition_id = Edition.current[:id] if edition_id.nil?
     categories = []
     Category.order(rod: :asc).each do |c|
+      books = Book.by_edition_and_category(
+        edition_id, c[:id]
+      ).order(title: :asc)
+
+      next if books.empty?
+
       categories << {
         id: c[:id],
         name: c[:name],
         group: I18n.t("activerecord.attributes.category.groups.#{c[:group]}"),
-        books: Book.by_edition_and_category(
-          edition_id, c[:id]
-        ).order(title: :asc)
+        books:
       }
     end
     categories
