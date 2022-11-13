201b4e6a2ad5314ebd28e9c79d2efb34a7fae452
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2022-11-14 00:09:54 +0100

    Displaying translators in Books index
    - Adding abbreviations to the author_types table
    - Displaying an abbraviated version of author types next to their names in the books#index view

---
+++ db/migrate/20221113220055_add_abbreviation_to_author_types.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class AddAbbreviationToAuthorTypes < ActiveRecord::Migration[7.0]
+  def up
+    add_column :author_types, :abbreviation, :string
+    role_abbrebiations = { '2' => 'Höf', '5' => 'Þýð', '4' => 'Myndir',
+                           '1' => 'Myndh', '7' => 'Les', '9' => 'Endurs',
+                           '10' => 'Skrás', '8' => 'Umsj', '6' => 'Ristj',
+                           '3' => 'Mndrtstj', '11' => 'Ljóð', '12' => 'Þýð.lj' }
+
+    AuthorType.all.each do |at|
+      next unless role_abbrebiations.key? at.id.to_s
+
+      at.abbreviation = role_abbrebiations[at.id.to_s]
+      at.save
+    end
+  end
+
+  def down
+    remove_column :author_types, :abbreviation
+  end
+end
