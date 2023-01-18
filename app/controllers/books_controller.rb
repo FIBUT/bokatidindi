ac37fea176d3712dd1edefe7725e70280008ee40
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-01-18 12:16:16 +0100

    Fixing accessibility issues
    - Redoing heading level for books
    - Fixing various aria tags
    - Adding missing title tag for all books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
 
       @books = Book.current if @books.nil?
       @books = @books.order(:title).page(params[:page])
+      @title_tag ||= 'Bókatíðindi - Allar bækur'
     end
   end
 
