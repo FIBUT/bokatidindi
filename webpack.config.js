7a1be06a57af8b5f11a81a9bcc76fd7165c0c2e2
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-01-20 10:58:51 +0100

    Fixing build
    So apparantly, webpack builds the Sass/CSS into the application.js file.
    
    I don't like webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
-  const path    = require("path")
-  const webpack = require("webpack")
-  
-  // Extracts CSS into .css file
-  const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-  // Removes exported JavaScript files from CSS-only entries
-  // in this example, entry.custom will create a corresponding empty custom.js file
-  const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-  
-  module.exports = {
-    mode: "production",
-    devtool: "source-map",
-    entry: {
-      application: [
-        "./app/javascript/application.js",
-        "./app/assets/stylesheets/application.scss",
-      ],
-      custom: './app/assets/stylesheets/custom.scss'
-    },
-    output: {
-      filename: "[name].js",
-      sourceMapFilename: "[file].map",
-      path: path.resolve(__dirname, "app/assets/builds"),
-    },
-    module: {
-      rules: [{
+const path    = require("path")
+const webpack = require("webpack")
+
+// Extracts CSS into .css file
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Removes exported JavaScript files from CSS-only entries
+// in this example, entry.custom will create a corresponding empty custom.js file
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    application: [
+      "./app/javascript/application.js",
+      "./app/assets/stylesheets/application.scss",
+    ],
+    custom: './app/assets/stylesheets/custom.scss'
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  module: {
+    rules: [
+      {
         test: /\.scss$/,
         use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
           {
-            loader: "style-loader" // creates style nodes from JS strings
+            loader: "sass-loader",
+            options: {
+              webpackImporter: true,
+            },
           },
-          {
-            loader: "css-loader" // translates CSS into CommonJS
-          },
-          {
-            loader: "sass-loader" // compiles Sass to CSS
-          },
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   resolve: {
     // Add additional file types
-    extensions: ['.js', '.scss', '.css'],
+    extensions: ['.scss', '.css'],
   },
   plugins: [
     new RemoveEmptyScriptsPlugin(),
