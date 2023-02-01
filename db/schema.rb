2e552aa0aee6ed32cf7eabe4bef0e47418e5d0ce
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-02-01 11:24:23 +0100

    Adding Pages model and ActiveAdmin resource
    - Adding a new model and migration for subpages
    - Adding the relevant ActiveAdmin resource
    - Calling the content from the relevant controllers based on the slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_164244) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_01_090629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_prewarm"
   enable_extension "pg_stat_statements"
@@ -256,6 +256,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_164244) do
     t.index ["online_date"], name: "index_editions_on_online_date"
     t.index ["opening_date"], name: "index_editions_on_opening_date"
     t.index ["print_date"], name: "index_editions_on_print_date"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
