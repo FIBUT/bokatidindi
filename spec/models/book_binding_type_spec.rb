5b68c5b55cbbbeb003c7945a84c0235f788e82ab
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-02-06 23:33:45 +0100

    Adding tests for ISSN and ISBN validations

--- spec/models/book_binding_type_spec.rb
+++ spec/models/book_binding_type_spec.rb
@@ -13,4 +13,48 @@ RSpec.describe BookBindingType, type: :model do
       ).to be_valid
     end
   end
+
+  it 'Validates ISBNs' do
+    binding_type = create(:binding_type, name: 'Bókabók', barcode_type: 'ISBN')
+
+    expect do
+      create(:book, book_binding_types: [
+               create(:book_binding_type, barcode: '0258-3771',
+                                          binding_type:),
+               create(:book_binding_type, barcode: '9771234567898',
+                                          binding_type:)
+             ])
+    end.to raise_error(ActiveRecord::RecordInvalid)
+
+    expect do
+      create(:book, book_binding_types: [
+               create(:book_binding_type, barcode: BookBindingType.random_isbn,
+                                          binding_type:),
+               create(:book_binding_type, barcode: BookBindingType.random_isbn,
+                                          binding_type:)
+             ])
+    end.not_to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'Validates ISSNs' do
+    binding_type = create(:binding_type, name: 'Tímarit', barcode_type: 'ISSN')
+
+    expect do
+      create(:book, book_binding_types: [
+               create(:book_binding_type, barcode: '0258-3771',
+                                          binding_type:),
+               create(:book_binding_type, barcode: '9771234567898',
+                                          binding_type:)
+             ])
+    end.not_to raise_error(ActiveRecord::RecordInvalid)
+
+    expect do
+      create(:book, book_binding_types: [
+               create(:book_binding_type, barcode: BookBindingType.random_isbn,
+                                          binding_type:),
+               create(:book_binding_type, barcode: BookBindingType.random_isbn,
+                                          binding_type:)
+             ])
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
 end
