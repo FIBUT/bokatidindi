fb4c3cb848fb355cbbac5bfeb3c9543a040e14b1
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-02-07 00:25:56 +0100

    Fixing random ISBN test

--- spec/models/book_binding_type_spec.rb
+++ spec/models/book_binding_type_spec.rb
@@ -4,13 +4,8 @@ require 'rails_helper'
 
 RSpec.describe BookBindingType, type: :model do
   it 'Generates valid random ISBN numbers for testing' do
-    book = Book.last
     200.times do
-      expect(
-        build(
-          :book_binding_type, book:, barcode: BookBindingType.random_isbn
-        )
-      ).to be_valid
+      expect(StdNum::ISBN.valid?(BookBindingType.random_isbn)).to be_truthy
     end
   end
 
