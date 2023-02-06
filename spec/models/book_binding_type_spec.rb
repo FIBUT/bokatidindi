42608ba3444914f06a519a9e30f8d6cc1378a616
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-02-07 00:05:09 +0100

    Adding duplicate checks to book_binding_type barcodes

--- spec/models/book_binding_type_spec.rb
+++ spec/models/book_binding_type_spec.rb
@@ -27,6 +27,18 @@ RSpec.describe BookBindingType, type: :model do
     end.to raise_error(ActiveRecord::RecordInvalid)
 
     expect do
+      isbns = [BookBindingType.random_isbn, BookBindingType.random_isbn]
+      2.times do
+        create(:book, book_binding_types: [
+                 create(:book_binding_type, barcode: isbns[0],
+                                            binding_type:),
+                 create(:book_binding_type, barcode: isbns[1],
+                                            binding_type:)
+               ])
+      end
+    end.to raise_error(ActiveRecord::RecordInvalid)
+
+    expect do
       create(:book, book_binding_types: [
                create(:book_binding_type, barcode: BookBindingType.random_isbn,
                                           binding_type:),
@@ -40,12 +52,14 @@ RSpec.describe BookBindingType, type: :model do
     binding_type = create(:binding_type, name: 'Tímarit', barcode_type: 'ISSN')
 
     expect do
-      create(:book, book_binding_types: [
-               create(:book_binding_type, barcode: '0258-3771',
-                                          binding_type:),
-               create(:book_binding_type, barcode: '9771234567898',
-                                          binding_type:)
-             ])
+      3.times do
+        create(:book, book_binding_types: [
+                 create(:book_binding_type, barcode: '0258-3771',
+                                            binding_type:),
+                 create(:book_binding_type, barcode: '9771234567898',
+                                            binding_type:)
+               ])
+      end
     end.not_to raise_error(ActiveRecord::RecordInvalid)
 
     expect do
