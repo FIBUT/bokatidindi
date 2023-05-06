15bbe49f1d54baffa10480b12eb4eb35674193db
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-05-06 17:34:39 +0200

    Making migration reversible

--- db/migrate/20230506151528_blockquote_length_to512.rb
+++ db/migrate/20230506151528_blockquote_length_to512.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class BlockquoteLengthTo512 < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :blockquotes, :quote, :string, limit: 512
+  end
+
+  def down
+    change_column :blockquotes, :quote, :string, limit: 255
   end
 end
