db9e1ce45e4ed355551aececf256816c7ad8e0cc
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-05-08 12:01:18 +0200

    Adding order scope for blockquotes

--- app/models/blockquote.rb
+++ app/models/blockquote.rb
@@ -6,6 +6,8 @@ class Blockquote < ApplicationRecord
 
   belongs_to :book
 
+  default_scope { order(id: :asc) }
+
   enum size: { medium: 0, large: 1, extra_large: 2 }
   enum location: { below_long_description: 0, below_book_description: 1 }
 end
