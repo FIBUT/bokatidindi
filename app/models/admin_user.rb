fd8e26fce5934c9f3e901c1b258af9dd653d9527
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2024-01-05 17:21:12 +0100

    Fixing ransackable attributes for admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -22,4 +22,16 @@ class AdminUser < ApplicationRecord
   validates :admin_user_publishers, length: { minimum: 1 }, if: :publisher?
   validates :admin_user_publishers, length: { is: 0 }, if: :admin?
   validates :admin_user_publishers, length: { is: 0 }, if: :guest?
+
+  def self.ransackable_attributes(_auth_object = nil)
+    ['created_at', 'current_sign_in_at', 'current_sign_in_ip', 'email',
+     'encrypted_password', 'failed_attempts', 'id', 'last_sign_in_at',
+     'last_sign_in_ip', 'locked_at', 'name', 'remember_created_at',
+     'reset_password_sent_at', 'reset_password_token', 'role', 'sign_in_count',
+     'unlock_token', 'updated_at']
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    ['admin_user_publishers', 'publishers']
+  end
 end
--- app/models/admin_user_publisher.rb
+++ app/models/admin_user_publisher.rb
@@ -3,4 +3,8 @@
 class AdminUserPublisher < ApplicationRecord
   belongs_to :admin_user
   belongs_to :publisher
+
+  def self.ransackable_attributes(_auth_object = nil)
+    ['admin_user_id', 'created_at', 'id', 'publisher_id', 'updated_at']
+  end
 end
