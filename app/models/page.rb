c09d2da0f09007eaeccf7a2576d4d0e46ff61bee
Alda Vigdis Skarphedinsdottir <aldavigdis@aldavigdis.is>
2024-06-05 01:45:16 +0200

    Fixing a 500 error for Pages (#397)
    I forgot to set the `ransackable_associations` and `ransackable_associations` methods for this one last year, so it was broken in ActiveAdmin. It should be fixed now.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Page < ApplicationRecord
+  def self.ransackable_associations(_auth_object = nil)
+    reflect_on_all_associations.map { |a| a.name.to_s }
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    column_names
+  end
 end
