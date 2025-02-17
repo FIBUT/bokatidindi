d1551dd2e2fe9fb736976759989438626d3565c2
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2025-02-17 13:37:48 +0100

    Improving layouts and preloading
    - JS and CSS is processed separately now, enabling us to only load CSS from assets processed via webpack instead of going the CSS-in-JS route, which was disgusting
    - Redoing responsive image definitions
    - Adding image preloads for books index and show views
    - Redoing the layout for the books show view, bringing it closer to the intended design
    - Providing an array of favicons
    - Processing/compressing SVG images

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,11 +4,6 @@ class WelcomeController < ApplicationController
   TILE_SIZES = [16, 32, 58, 64, 76, 80, 114, 120, 128, 144, 152, 167, 180,
                 192, 256, 512].freeze
 
-  META_DESCRIPTION = 'Bókatíðindi hafa síðan 1928 veitt yfirlit yfir '\
-                     'útgáfu ársins fyrir bókaunnendur. Þar má alltaf '\
-                     'nálgast upplýsingar um þær bækur sem gefnar eru '\
-                     'út á hverju ári.'
-
   def index
     expires_in 1.hour, public: true
 
