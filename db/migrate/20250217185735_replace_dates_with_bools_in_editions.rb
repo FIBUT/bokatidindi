0ae0060f8e000de7941dd08685b0d8d5f3535801
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2025-02-18 14:39:05 +0100

    Correcting migration testing

--- db/migrate/20250217185735_replace_dates_with_bools_in_editions.rb
+++ db/migrate/20250217185735_replace_dates_with_bools_in_editions.rb
@@ -7,19 +7,6 @@ class ReplaceDatesWithBoolsInEditions < ActiveRecord::Migration[7.0]
     add_index :editions, :online
     add_index :editions, :open_to_web_registrations
     add_index :editions, :open_to_print_registrations
-
-    current_edition = Edition.current_edition
-    current_edition[:online] = true
-    current_edition.save
-
-    last_edition = Edition.last
-    last_edition[:open_to_web_registrations] = true
-    last_edition[:open_to_print_registrations] = true
-    last_edition.save
-
-    # remove_column :editions, :opening_date
-    # remove_column :editions, :online_date
-    # remove_column :editions, :closing_date
   end
 
   def down
