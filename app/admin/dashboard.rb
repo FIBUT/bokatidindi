6cfef9767db9cce3bb67ff1d361f8d8a8dbcee59
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2025-02-18 23:31:36 +0100

    Only displaying edition deadlines if there is an active one

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,85 +6,87 @@ ActiveAdmin.register_page 'Dashboard' do
   content title: proc { I18n.t('active_admin.dashboard') } do
     edition = Edition.current.last
 
-    panel 'Frestir og dagsetningar' do
-      table class: 'edition-dates-table' do
-        if edition.opening_date
-          tr do
-            th scope: 'row' do
-              'Opnað fyrir skráningar'
+    if edition
+      panel 'Frestir og dagsetningar' do
+        table class: 'edition-dates-table' do
+          if edition.opening_date
+            tr do
+              th scope: 'row' do
+                'Opnað fyrir skráningar'
+              end
+              td do
+                edition&.opening_date&.strftime('%d.%m.%Y')
+              end
+              td class: 'description' do
+                'Opnað fyrir skráningar í þennan árgang Bókatíðinda.'
+              end
             end
-            td do
-              edition&.opening_date&.strftime('%d.%m.%Y')
+          end
+          if edition.online_date
+            tr do
+              th scope: 'row' do
+                'Árgangur birtist á vef'
+              end
+              td do
+                edition&.online_date&.strftime('%d.%m.%Y')
+              end
+              td class: 'Description' do
+                'Þessi árgangur Bókatíðinda birtist á vefnum.'
+              end
             end
-            td class: 'description' do
-              'Opnað fyrir skráningar í þennan árgang Bókatíðinda.'
+          end
+          if edition.closing_date
+            tr do
+              th scope: 'row' do
+                'Lokað fyrir skráningar'
+              end
+              td do
+                edition&.closing_date&.strftime('%d.%m.%Y')
+              end
+              td class: 'description' do
+                'Lokað fyrir skráningar fram að frágangi prentútgáfu.'
+              end
+            end
+          end
+          if edition.print_date
+            tr do
+              th scope: 'row' do
+                'Gengið frá prentútgáfu'
+              end
+              td do
+                edition&.print_date&.strftime('%d.%m.%Y')
+              end
+              td class: 'Description' do
+                'Eftir þessa dagsetningu er opnað aftur fyrir skráningar í '\
+                'vefútgáfu.'
+              end
             end
           end
         end
-        if edition.online_date
-          tr do
-            th scope: 'row' do
-              'Árgangur birtist á vef'
-            end
-            td do
-              edition&.online_date&.strftime('%d.%m.%Y')
-            end
-            td class: 'Description' do
-              'Þessi árgangur Bókatíðinda birtist á vefnum.'
-            end
-          end
-        end
-        if edition.closing_date
-          tr do
-            th scope: 'row' do
-              'Lokað fyrir skráningar'
-            end
-            td do
-              edition&.closing_date&.strftime('%d.%m.%Y')
-            end
-            td class: 'description' do
-              'Lokað fyrir skráningar fram að frágangi prentútgáfu.'
-            end
-          end
-        end
-        if edition.print_date
-          tr do
-            th scope: 'row' do
-              'Gengið frá prentútgáfu'
-            end
-            td do
-              edition&.print_date&.strftime('%d.%m.%Y')
-            end
-            td class: 'Description' do
-              'Eftir þessa dagsetningu er opnað aftur fyrir skráningar í '\
-              'vefútgáfu.'
-            end
-          end
-        end
+        para 'Athugið að þessar dagsetningar geta breyst án fyrirvara.'
       end
-      para 'Athugið að þessar dagsetningar geta breyst án fyrirvara.'
-    end
 
-    panel 'Samtalstölur skráninga' do
-      table class: 'edition-stats-table' do
-        tr do
-          th scope: 'row' do
-            'Heildarfjöldi bóka'
+      panel 'Samtalstölur skráninga' do
+        table class: 'edition-stats-table' do
+          tr do
+            th scope: 'row' do
+              'Heildarfjöldi bóka'
+            end
+            td do
+              BookEdition.where(edition_id: edition.id).count
+            end
           end
-          td do
-            BookEdition.where(edition_id: edition.id).count
-          end
-        end
-        tr do
-          th scope: 'row' do
-            'Heildarfjöldi skráninga'
-          end
-          td do
-            BookEditionCategory.includes(
-              :book_edition
-            ).where(
-              book_edition: { edition_id: 2 }
-            ).count
+          tr do
+            th scope: 'row' do
+              'Heildarfjöldi skráninga'
+            end
+            td do
+              BookEditionCategory.includes(
+                :book_edition
+              ).where(
+                book_edition: { edition_id: 2 }
+              ).count
+            end
           end
         end
       end
