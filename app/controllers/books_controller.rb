007a4138bc28a05454c4006eb43a2f7f9fa10841
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2025-03-04 12:15:16 +0100

    Removing "current" condition for author listings
    We were returning a 404 error for authors who's books aren't in the current edition. This fixes that.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -100,7 +100,7 @@ class BooksController < ApplicationController
 
     author_books = Book.by_author(@author)
 
-    @books = author_books.current.with_attached_cover_image.order(:title)
+    @books = author_books.with_attached_cover_image.order(:title)
                          .page(params[:page])
 
     @books_from_old_editions = author_books - @books
