e8241c5a679d55f7ecdf7e8fb794a071e0c924d0
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2026-09-24 20:11:42 +0200

    Commenting out blocked IPs and removing custom error

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,10 +2,10 @@
 
 require 'rack/attack'
 
-Rack::Attack.blocklist_ip '40.76.0.0/14'
-Rack::Attack.blocklist_ip '207.46.0.0/19'
-Rack::Attack.blocklist_ip '52.160.0.0/11'
-Rack::Attack.blocklist_ip '157.55.0.0/16'
+# Rack::Attack.blocklist_ip '40.76.0.0/14'
+# Rack::Attack.blocklist_ip '207.46.0.0/19'
+# Rack::Attack.blocklist_ip '52.160.0.0/11'
+# Rack::Attack.blocklist_ip '157.55.0.0/16'
 
 Rack::Attack.blocklist('block access to WP related URLs') do |request|
   request.path.start_with? '/wp-admin'
@@ -16,12 +16,4 @@ end
 
 Rack::Attack.blocklist('block access to cofiguration related urls') do |request|
   request.path.include? '.env'
-end
-
-Rack::Attack.blocklisted_responder = lambda do |_request|
-  [
-    403,
-    { 'Content-Type' => 'text/html' },
-    File.read(Rails.root.join('public/403.html'))
-  ]
 end
