69d4527f6a5e67f93ab2e257f386ea2bf5ec8321
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2026-09-25 16:37:23 +0200

    Uncommenting Azure IPs that keep bombing our search endpoint

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,10 +16,7 @@ class BooksController < ApplicationController
                    '(max-width: 420px) 260px'
 
     if params[:search]
-      unless search_key_okay
-        sleep 25
-        return render_four_oh_three
-      end
+      return render_four_oh_three unless search_key_okay
 
       render_search
       book_results = @books + @books_from_old_editions
